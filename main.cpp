fd291a03a55d6ad62665dadd25f70f1c0d2b9cd9
yingongjun <yingongjun@hotmain.com>
2017-10-31 22:05:33 +0800

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -2,37 +2,7 @@
 #include "factory.h"
 
 int main() {
-#ifdef DEBUG_NEURAL
-	std::shared_ptr<Neural> pe = Singelon<Neural>::instance();
-	pe->setNeuralPath("G:\\neuron.txt");
-	pe->setMinAndMaxPath("G:\\min_max.txt");
-	pe->setDatePath("G:\\data2.csv");
 
-	pe->readData();
-	pe->initBPNework();
-	pe->trainNetwork();
-	pe->writeNeuron();
-	pe->writeMinAndMax();
-
-	double input1[] = { 1,	5,	2,	3,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0,
-		0,	0,	0,	1,	0,	1,	1,	0,	0,	0,	0,	1,	1,	1,	0,	0,	1,	1,	0,
-		0,	0,	0,	0,	1,	0,	0,	0,
-	};
-	printf("%lf \n", pe->result(input1));
-
-	double input2[] = { 2,	6,	4,	3,	1,	0,	0,	0,	0,	1,	1,	0,	1,	0,	0,	0,
-		0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	1,	0,	0,	1,	0,	0,	0,	1,	1,	0,
-		0,	0,	0,	0,	1,	0,
-	};
-
-	double input3[] = { 2,	5,	1,	3,	3,	0,	0,	0,	0,	1,	1,	0,	1,	0,	1,	0,
-		1,	0,	0,	0,	1,	0,	1,	0,	0,	0,	1,	0,	0,	1,	0,	0,	0,	1,	0,	0,
-		0,	0,	0,	0,	1,	0,
-	};
-	printf("%lf \n", pe->result(input2));
-	printf("%lf \n", pe->result(input3));
-#endif
-#ifdef DEBUG_FACTORY
 	PersonFactory pf;
 	for(int i=0;i<20;i++)
 	std::cout << pf.generateIntRandom(0,1) << std::endl;
